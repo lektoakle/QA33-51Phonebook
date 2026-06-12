8c8703a125167a99d2bf21d490e8b273f52605b4
lektoakle <oknezrob@gmail.com>
2026-06-12 23:38:42 +0300

    cleaned chrome and firefox driver options

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -32,35 +32,54 @@ public class ApplicationManager {
 
     public void init() {
         if (browser.equals(Browser.CHROME.browserName())) {
-            ChromeOptions options = new ChromeOptions();
+            try {
+                ChromeOptions options = new ChromeOptions();
 //            String headless = System.getProperty("headless");
 //
 //            if ("true".equalsIgnoreCase(headless)) {
 //                options.addArguments("--headless=new");
 //            }
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--window-size=1920,1080");
 
-            options.addArguments("--disable-features=UseOzonePlatform");
-            options.addArguments("--disable-features=VizDisplayCompositor");
+                options.setBinary("/opt/chrome/chrome-linux64/chrome");
 
-            options.addArguments("--user-data-dir=/tmp/chrome-jenkins");
-            options.setBinary("/opt/chrome/chrome-linux64/chrome");
-            System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-            System.setProperty("webdriver.chrome.verboseLogging", "true");
-            System.setProperty("webdriver.chrome.logfile", "/tmp/chromedriver.log");
+                options.addArguments("--window-size=1920,1080");
+                options.addArguments("--headless=new");
 
-            wd = new ChromeDriver(options);
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+//            options.addArguments("--disable-gpu");
+//                options.addArguments("--disable-features=UseOzonePlatform");
+//            options.addArguments("--disable-features=VizDisplayCompositor");
+//            options.addArguments("--user-data-dir=/tmp/chrome-" + System.nanoTime());
+//            options.addArguments("--user-data-dir=/tmp/chrome-jenkins");
+//
+//                System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+//                System.setProperty("webdriver.chrome.verboseLogging", "true");
+//                System.setProperty("webdriver.chrome.logfile", "/tmp/chromedriver.log");
 
-            logger.info("All tests run in Chrome browser");
+                wd = new ChromeDriver(options);
+                System.out.println("Driver created: " + wd);
+                logger.info("All tests run in Chrome browser, driver  created " + wd);
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw e;
+            }
         } else if (browser.equals(Browser.FIREFOX.browserName())) {
             FirefoxOptions options = new FirefoxOptions();
             options.setBinary("/opt/firefox/firefox");
             options.addArguments("--headless");
+//            options.addArguments("--headless=new");
+//            options.addArguments("--no-sandbox");
+//            options.addArguments("--disable-gpu");
+            System.setProperty("webdriver.firefox.logfile", "/tmp/geckodriver.log");
+            System.setProperty("webdriver.firefox.verboseLogging", "true");
+            System.setProperty(
+                    "webdriver.gecko.driver",
+                    "/usr/local/bin/geckodriver-clean"
+            );
             wd = new FirefoxDriver(options);
+            System.out.println("driver in setup = " + wd);
+            System.out.println("browser = firefox");
             logger.info("All tests run in Firefox browser");
         } else if (browser.equals(Browser.EDGE.browserName())) {
             wd = new EdgeDriver();
@@ -74,8 +93,10 @@ public class ApplicationManager {
         }
 
 
-        wd.manage().window().maximize();
+//        wd.manage().window().maximize();
+        System.out.println("navigate to the app");
         wd.navigate().to("https://telranedu.web.app/");
+        System.out.println("done navigating to the app");
         logger.info("url:" + wd.getCurrentUrl());
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         WebDriverListener webDriverListener = new ListenerWD();
